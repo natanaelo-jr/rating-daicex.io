726793a4d6c9b9fe0970bf8b0bee12d54ef8937e
Natanael Júnior <135863264+natanaelo-jr@users.noreply.github.com>
2024-06-14 23:19:05 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 let root = document.getElementById('root')
+let sorted = document.getElementById('sorted')
 
 
 function fetchData(){
@@ -11,10 +12,27 @@ function fetchData(){
             return res.json();
         })
         .then((data) => {
-            data.forEach(element => root.insertAdjacentHTML('beforebegin', `<tr><td class="text-zinc-500 p-2">${element.name}</td><td  class="text-zinc-500">${element.rating}</td></tr>`));
-        }
+            
+            data.forEach(element => root.insertAdjacentHTML('beforebegin', `<tr class="hover:bg-zinc-800 hover:text-zinc-100"><td class="text-zinc-500 p-2">${element.name}</td><td  class="text-zinc-500">${element.rating}</td></tr>`));        }
         )
         .catch((error) => 
             console.error("Nao achei"));
 }
+function fetchData2(){
+    fetch('./data.json')
+        .then((res) =>{
+            if (!res.ok){
+                throw new Error
+                    ('error: Status: ${res.status}');
+            }
+            return res.json();
+        })
+        .then((data) => {
+            data.sort((a, b) => b.rating - a.rating)
+            data.forEach(element => sorted.insertAdjacentHTML('beforebegin', `<tr class="hover:bg-zinc-800 hover:text-zinc-100"><td class="text-zinc-500 p-2">${element.name}</td><td  class="text-zinc-500">${element.rating}</td></tr>`));        }
+        )
+        .catch((error) => 
+            console.error("Nao achei"));
+    }
 fetchData()
+fetchData2()
